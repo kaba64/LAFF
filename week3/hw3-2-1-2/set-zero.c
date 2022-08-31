4e87c3944649fef9122786509cfea0362c08d8c3
Kazem <bazesefidpar64@gmail.com>
2022-08-31 12:36:21 +0200

    add SetZero_D function

--- week3/hw3-2-1-2/set-zero.c
+++ week3/hw3-2-1-2/set-zero.c
@@ -5,7 +5,8 @@
 #define PRINT_DATA
 
 // Declaration of local prototypes.
-int ZeroMatrix_unb(FLA_Obj A);
+int ZeroMatrix_D_unb(FLA_Obj A);
+void SetZero_D(double* x,int m, int n);
 
 int main( int argc, char *argv[] ) {
   int m_A, n_A;
@@ -15,7 +16,7 @@ int main( int argc, char *argv[] ) {
   scanf("%d%d",&m_A,&n_A);
   FLA_Init();
   FLA_Obj_create(FLA_DOUBLE,m_A,n_A,0,0,&A); //FLA_INT
-  ZeroMatrix_unb(A);
+  ZeroMatrix_D_unb(A);
 #ifdef PRINT_DATA
   FLA_Obj_show( " A = [ ", A, "%le", " ];" );
 #endif
@@ -32,7 +33,7 @@ int main( int argc, char *argv[] ) {
    Programmed by: Kazem Bazesefidpar
    kazemba@kth.se
 */
-int ZeroMatrix_unb(FLA_Obj A)
+int ZeroMatrix_D_unb(FLA_Obj A)
 {
   FLA_Obj AL,AR,A0,a1,A2;
   double  * buff_a1;
@@ -51,10 +52,7 @@ int ZeroMatrix_unb(FLA_Obj A)
     buff_a1 = ( double * ) FLA_Obj_buffer_at_view( a1 );
     m_A    = FLA_Obj_length(a1);
     n_A    = FLA_Obj_width (a1);
-    for(j=0;j<m_A;++j){
-      buff_a1[j]= (double) count;
-      //++count
-    }
+    SetZero_D(buff_a1,m_A,n_A);
     /*------------------------------------------------------------*/
 
     FLA_Cont_with_1x3_to_1x2( &AL,  /**/ &AR,        A0, a1, /**/ A2,
@@ -63,4 +61,21 @@ int ZeroMatrix_unb(FLA_Obj A)
   }
 
   return FLA_SUCCESS;
+}
+
+void SetZero_D(double * x,int m, int n){
+  int i,j;
+  int count;
+  count=0;
+  if(n==1){
+    for(j=0;j<m;++j){
+      x[j]=(double)count;
+      ++count;
+    }
+  }else{
+    for(i=0;i<n;++i){
+      x[i]=(double)count;
+      ++count;
+    }
+  }
 }
